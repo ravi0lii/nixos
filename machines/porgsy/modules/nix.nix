67ee53e887a4c79981d0e37080c949a1f42cc023
ravi0li <moritz@howtoflex.club>
2021-07-02 00:36:47 +0200

    enable nix flakes

--- machines/porgsy/modules/nix.nix
+++ machines/porgsy/modules/nix.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # TODO: Should this be moved into an own module?
   nix = {
+    # nixUnstable is required for nix flakes
+    package = pkgs.nixUnstable;
+
+    # Enable nix flakes
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+
     gc = {
       automatic = true;
       dates = "weekly";
