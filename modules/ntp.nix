753c46fe1894a3ec5abda88feeddaa2da8ea9810
ravi0li <moritz@howtoflex.club>
2021-07-05 22:37:36 +0200

    switch to chrony

--- modules/ntp.nix
+++ modules/ntp.nix
@@ -1,5 +1,8 @@
 { ... }:
 
 {
-  services.ntp.enable = true;
+  # ntpd hangs on shutdown, see https://github.com/NixOS/nixpkgs/issues/38627
+  # Until this is fixed, use chrony instead of ntpd
+  # services.ntp.enable = true;
+  services.chrony.enable = true;
 }
